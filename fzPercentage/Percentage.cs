c5e1100eb0b990ea2899e9745371f0c2ec9acc88
Filip Zawada <fzawada@gmail.com>
2013-08-03 01:03:26 +0200

    Added first tests: equality

--- fzPercentage/Percentage.cs
+++ fzPercentage/Percentage.cs
@@ -8,5 +8,27 @@ namespace fzPercentage
 {
     public class Percentage
     {
+        private readonly double value;
+
+        public Percentage(double value)
+        {
+            this.value = value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as Percentage;
+            if (other == null)
+            {
+                return false;
+            }
+
+            return value.Equals(other.value);
+        }
+
+        public override int GetHashCode()
+        {
+            return value.GetHashCode();
+        }
     }
 }
